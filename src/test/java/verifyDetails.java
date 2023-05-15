eac04f3617b6e0d84f23c72afa32d6e5f8fac6c0
Priyanka <priyankaverma02895@gmail.com>
2023-05-15 18:59:08 -0400

    username

--- src/test/java/verifyDetails.java
+++ src/test/java/verifyDetails.java
@@ -12,10 +12,8 @@ public class verifyDetails extends baseTest {
     public void details()
     {
         RestAssured.baseURI = secrets.getBaseURI();
-        String username = secrets.getUsername();
-        String password = secrets.getPassword();
 
-        String res1 = given().auth().preemptive().basic(username, password).pathParam(values.getIssueKey(), values.getIssueID())
+        String res1 = given().auth().preemptive().basic(secrets.getUsername(), secrets.getPassword()).pathParam(values.getIssueKey(), values.getIssueID())
                 .queryParam(values.getQueryParamKey(), values.getQueryParamValue())
                 .log().all()
                 .when().get(values.getResource())
